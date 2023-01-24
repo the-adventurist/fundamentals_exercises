1504cfbc9c08bc112eeb9a211744070dc21d0f0a
Daniel Yordanov <theface11311@gmail.com>
2023-01-24 23:49:48 +0200

    Repaired josephus_permutation.py with 100/100 in judge.

--- josephus_permutation.py
+++ josephus_permutation.py
@@ -1,10 +1,17 @@
-people = [int(x) for x in input().split()]
+people = [x for x in input().split()]
+killed_people = []
 k = int(input())
-k_progress = k
+n = 0
 while people:
-
-    for to_be_killed in people:
-        k_progress += 1
-        if k_progress % k == 0 and k <= len(people):
-            print(to_be_killed)
+    for to_be_killed in range(len(people)):
+        n += 1
+        if n == k:
+            n = 0
+            killed_guy = people.pop(to_be_killed)
+            killed_people.append(killed_guy)
+            after_killed_guy = people[to_be_killed:]
+            before_killed_guy = people[:to_be_killed]
+            people = after_killed_guy + before_killed_guy
             break
+print(f"[{','.join(killed_people)}]")
+
