b19f5e934fd85be2bbf4ac945679690754fe17d7
Daniel Yordanov <theface11311@gmail.com>
2023-01-29 11:34:36 +0200

    Entirely new logic for tic_tac_toy.py

--- tic_tac_toy.py
+++ tic_tac_toy.py
@@ -1,10 +1,27 @@
-n = 3
-matrix = []
-for _ in range(3):
-    row = input()
-    line = []
-    for symbol in row:
-        if symbol != ' ':
-            line.append(symbol)
-    matrix.append(line)
-for
+result = 'Draw!'
+first_won = False
+second_won = False
+list_1_horizontal = input().split()
+list_2_horizontal = input().split()
+list_3_horizontal = input().split()
+common_list = list_1_horizontal + list_2_horizontal + list_3_horizontal
+common_list += list_1_horizontal[0], list_2_horizontal[0], list_3_horizontal[0]
+common_list += list_1_horizontal[1], list_2_horizontal[1], list_3_horizontal[1]
+common_list += list_1_horizontal[2], list_2_horizontal[2], list_3_horizontal[2]
+common_list += list_1_horizontal[0], list_2_horizontal[1], list_3_horizontal[2]
+common_list += list_1_horizontal[2], list_2_horizontal[1], list_3_horizontal[0]
+final_list = []
+while common_list:
+    current_triple = ''
+    current_element = ''
+    for i in range(3):
+        current_element = common_list.pop(0)
+        current_triple += current_element
+    final_list.append(current_triple)
+
+if '111' in final_list:
+    print('First player won')
+elif '222' in final_list:
+    print('Second player won')
+else:
+    print(result)
