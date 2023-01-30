de5eb712810694733f0f63d53a82552795467644
Daniel Yordanov <theface11311@gmail.com>
2023-01-30 21:19:34 +0200

    Added 'add and subtract' task.Changed

--- add_and_subtract.py
+++ add_and_subtract.py
@@ -1,17 +1,20 @@
-def add_and_subtract(first, second, third):
-    return first, second, third
-
-
 def sum_numbers(first, second):
-    pass
+    return first + second
 
 
 def subtract(result, third):
-    pass
+    return result - third
+
+
+def add_and_subtract(first, second, third):
+    result_of_addition = sum_numbers(first, second)
+    result_of_subtraction = subtract(result_of_addition, third)
+    return result_of_subtraction
 
 
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
-add_and_subtract(first_number, second_number, third_number)
 
+
+print(add_and_subtract(first_number, second_number, third_number))
